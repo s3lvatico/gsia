0400440c849f82993954c33c84b40f2e1f8e163f
S3lvatico <s3lvatico@gmail.com>
2019-06-14 21:24:43 +0200

    custom logging

--- src/main/java/tech/jore/gsia/web/NewYearController.java
+++ src/main/java/tech/jore/gsia/web/NewYearController.java
@@ -1,16 +1,15 @@
 package tech.jore.gsia.web;
 
-
 import java.util.Calendar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 import tech.jore.gsia.domain.Anno;
-
 
 @Controller
 @RequestMapping("/newYear")
@@ -20,8 +19,14 @@ public class NewYearController {
 	@GetMapping
 	public String prepareNewYear(Model model) {
 		int y = Calendar.getInstance().get(Calendar.YEAR);
-		log.debug("new year planning requested for year {}", y);
+		log.info("new year planning requested for year {}", y);
 		model.addAttribute("anno", new Anno(y));
 		return "editYear";
+	}
+
+	@PostMapping
+	public String processNewYear(Anno anno) {
+		log.debug("the proecessed Anno : {}", anno);
+		return "home";
 	}
 }
