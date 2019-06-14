824b6e86fef196a7ec4166df9a99769ac55877f6
Simone Monotti <s3lvatico@gmail.com>
2019-06-14 17:33:29 +0200

    first commit

---
+++ src/main/java/tech/jore/gsia/GsiaApplication.java
@@ -0,0 +1,13 @@
+package tech.jore.gsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsiaApplication.class, args);
+	}
+
+}
