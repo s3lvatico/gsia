347cd466ddcfcdadbb3c5616fe5c4da3bd2325ef
Simone Monotti <s3lvatico@gmail.com>
2019-06-14 18:02:36 +0200

    test sistemati

--- src/test/java/tech/jore/gsia/GsiaApplicationTests.java
+++ src/test/java/tech/jore/gsia/GsiaApplicationTests.java
@@ -1,35 +1,18 @@
 package tech.jore.gsia;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GsiaApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
-	
 	@Test
 	public void contextLoads() {
-	}
-	
-	@Test 
-	public void testHomePage() throws Exception {
-		mockMvc.perform(get("/"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("home"))
-		.andExpect(content().string(containsString("gsia-home")));
 	}
 
 }
