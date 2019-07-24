3163c7ed34f185d3f99c4a42676da21f4a5c4b2f
Simone Monotti <simone.monotti@metisoft.it>
2019-07-24 13:01:03 +0200

    aggiunto primo oggetto del modello

---
+++ src/main/java/tech/gmnz/sandbox/torta/Ingredient.java
@@ -0,0 +1,17 @@
+package tech.gmnz.sandbox.torta;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+
+	public enum Type {
+		TORTA, VEGETALI, PROTEINE, FORMAGGIO
+	}
+	
+	private final String id;
+	private final String name;
+	private final Type type;
+}
